584bc06583c5c76cb8be90f17846ab015c3a2ab9
Fernando Arteaga <fernan2ac11@gmail.com>
2024-02-02 14:42:38 -0500

    worked on the Home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fernandoarteaga.dev',
-  integrations: [mdx(), sitemap(), icon()]
+  integrations: [mdx(), sitemap(), icon(), tailwind()]
 });
