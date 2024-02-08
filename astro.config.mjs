0aed8ad46ce6122e1ee097022fbfef4a712745c6
Fernando Arteaga <fernan2ac11@gmail.com>
2024-02-07 20:34:11 -0500

    add blog psycopg-vs-asyncpg (#9)
    * add blog psycopg-vs-asyncpg
    * add graphs images

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fernandoarteaga.dev',
-  // site: 'https://fernandoarteaga.github.io',
   integrations: [mdx(), sitemap(), icon(), tailwind()]
 });
